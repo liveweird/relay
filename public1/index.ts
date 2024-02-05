59f84612d2480b7ed5021a287f437852f84e81bf
Sebastian Gebski <sebastian@gebski.net>
2024-02-05 19:15:15 +0100

    Migrated from Bun to Node

--- public1/index.ts
+++ public1/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 8080;
+
+dotenv.config();
 
 const private1Host = process.env.PRIVATE1_HOST;
 const private1Port = process.env.PRIVATE1_PORT;
