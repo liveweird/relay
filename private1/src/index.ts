d462e0d911d246a55a29680f44f2fced9ba61c43
Sebastian Gebski <sebastian@gebski.net>
2024-02-21 14:28:30 +0100

    Logging object details

--- private1/src/index.ts
+++ private1/src/index.ts
@@ -46,7 +46,7 @@ async function getItemsPrivate2(): Promise<Item[]> {
     
     return fetch(request)
         .then(response => {
-            console.log(`Unparsed: ${response}`);
+            console.log(`Unparsed: ${JSON.stringify(response)}`);
             return (response as ItemsResponse).json();
         })
         .then(items => {
